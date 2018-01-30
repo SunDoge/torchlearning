4808bb71746103fada6f0cad56bdba9210fd2aab
chenyaofo <chenyaofo@gmail.com>
2018-01-30 19:10:58 +0800

    change engine.train structure.

--- engine.py
+++ engine.py
@@ -2,6 +2,7 @@ from .meter import *
 
 import torch
 
+
 class AttrDict(dict):
     def __init__(self, *args, **kwargs):
         super(AttrDict, self).__init__(*args, **kwargs)
@@ -9,11 +10,11 @@ class AttrDict(dict):
 
 
 class DefaultClassificationEngine(object):
-    def __init__(self,hooks={}):
+    def __init__(self, hooks={}):
         self.hooks = hooks
-        self.hook("on_init",None)
+        self.hook("on_init", None)
 
-        self.cudable =torch.cuda.is_available()
+        self.cudable = torch.cuda.is_available()
 
     def hook(self, name, state):
         if name in self.hooks:
@@ -23,55 +24,19 @@ class DefaultClassificationEngine(object):
                 else:
                     f(state)
 
-    def train(self, network, iterator, maxepoch, optimizer):
+    def train(self, network, iterator, optimizer, epoch=0):
         state = {
-            "engine":self,
+            "engine": self,
             'network': network,
             'iterator': iterator,
-            'maxepoch': maxepoch,
             'optimizer': optimizer,
-            'epoch': 0,
+            'epoch': epoch,
             't': 0,
             'train': True,
         }
 
         self.hook('on_start', state)
-        while state['epoch'] < state['maxepoch']:
-            self.hook('on_start_epoch', state)
-            for sample in state['iterator']:
-                state['sample'] = sample
-                self.hook('on_sample', state)
-
-                def closure():
-                    loss, output = state['network'](state['sample'])
-                    state['output'] = output
-                    state['loss'] = loss
-                    loss.backward()
-                    self.hook('on_forward', state)
-                    # to free memory in save_for_backward
-                    state['output'] = None
-                    state['loss'] = None
-                    return loss
-
-                state['optimizer'].zero_grad()
-                state['optimizer'].step(closure)
-                self.hook('on_update', state)
-                state['t'] += 1
-            state['epoch'] += 1
-            self.hook('on_end_epoch', state)
-        self.hook('on_end', state)
-        return state
-
-    def validate(self, network, iterator):
-        state = {
-            "engine": self,
-            'network': network,
-            'iterator': iterator,
-            't': 0,
-            'train': False,
-        }
-
-        self.hook('on_start', state)
+        self.hook('on_start_epoch', state)
         for sample in state['iterator']:
             state['sample'] = sample
             self.hook('on_sample', state)
@@ -80,15 +45,52 @@ class DefaultClassificationEngine(object):
                 loss, output = state['network'](state['sample'])
                 state['output'] = output
                 state['loss'] = loss
+                loss.backward()
                 self.hook('on_forward', state)
                 # to free memory in save_for_backward
                 state['output'] = None
                 state['loss'] = None
+                return loss
 
-            closure()
+            state['optimizer'].zero_grad()
+            state['optimizer'].step(closure)
+            self.hook('on_update', state)
             state['t'] += 1
+        state['epoch'] += 1
+        self.hook('on_end_epoch', state)
+
         self.hook('on_end', state)
         return state
 
-    def destroy(self):
-        self.hook("on_destroy", None)
+
+def validate(self, network, iterator):
+    state = {
+        "engine": self,
+        'network': network,
+        'iterator': iterator,
+        't': 0,
+        'train': False,
+    }
+
+    self.hook('on_start', state)
+    for sample in state['iterator']:
+        state['sample'] = sample
+        self.hook('on_sample', state)
+
+        def closure():
+            loss, output = state['network'](state['sample'])
+            state['output'] = output
+            state['loss'] = loss
+            self.hook('on_forward', state)
+            # to free memory in save_for_backward
+            state['output'] = None
+            state['loss'] = None
+
+        closure()
+        state['t'] += 1
+    self.hook('on_end', state)
+    return state
+
+
+def destroy(self):
+    self.hook("on_destroy", None)
