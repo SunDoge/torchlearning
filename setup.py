840472e7bdcf32a0154799b97dc07eb7e904aaa3
chenyaofo <chenyaofo@gmail.com>
2018-08-28 12:57:16 +0800

    add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="torchlearning",
+    version="0.0.1",
+    description="A deeplearning utility library for Pytorch",
+    url="https://github.com/chenyaofo/torchlearning",
+    author="chenyaofo",
+    author_email="chenyaofo@gmail.com",
+    packages=["torchlearning"],
+)
